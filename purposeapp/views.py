d3884fceea7b0c2aed488e0e73b6dd8fda9e9c01
Emmanuel Lihanda <emmanuelihanda@gmail.com>
2026-02-09 22:55:52 +0300

    Set up of edit_photo.html, profile.html,upload_photo.html and update on home.html and base.html

--- purposeapp/views.py
+++ purposeapp/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
-
 # Create your views here.
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Photo, Like
+from django.contrib.auth.decorators import login_required
+
+def home(request):
+    photos = Photo.objects.all().order_by('-created_at')
+    return render(request, 'gallery/home.html', {'photos': photos})
+def photo_detail(request, pk):
+    photo = get_object_or_404(Photo, pk=pk)
+    return render(request, 'gallery/photo_detail.html', {'photo': photo})
+@login_required
+def react_photo(request, pk, value):
+    photo = get_object_or_404(Photo, pk=pk)
+    Like.objects.update_or_create(
+        user=request.user,
+        photo=photo,
+        defaults={'value': value}
+    )
+    return redirect('photo_detail', pk=pk)
+
